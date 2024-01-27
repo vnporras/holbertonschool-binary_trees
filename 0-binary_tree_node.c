ca5c3d73458f437ccc56657fe3174111011c7b12
vnporras <vnporras1028@gmail.com>
2024-01-27 06:23:41 -0800

    create the function that create a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,9 +4,21 @@
  * binary_tree_node - Is a function that creates a binary tree node.
  * @parent: is a pointer to the parent node of the node to create.
  * @value: Is a value to put in the nee node
- * Return: the binary tree.
+ * Return: the new node..
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode = NULL;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return (newNode);
 }
