ac58e6320ea45da0bdc8b2864667dd3579097b10
vnporras <vnporras1028@gmail.com>
2024-01-27 06:48:27 -0800

    modified the line 7

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  * *binary_tree_insert_left - inserts a node as the left-child of another node.
  * @parent: Is a pointer to the node to insert the left-child in.
  * @value: Is a value to store in the mew node.
- * Return: A pointer to the created node, or NULL on failure or if parent is NULL.
+ * Return: A pointer to the created node.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
