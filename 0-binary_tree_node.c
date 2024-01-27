83ed1fbef9b6276c88c5d5fe2fd8c0201302152e
vnporras <vnporras1028@gmail.com>
2024-01-27 06:05:48 -0800

    create the new files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Is a function that creates a binary tree node.
+ * @parent: is a pointer to the parent node of the node to create.
+ * @value: Is a value to put in the nee node
+ * Return: the binary tree.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
